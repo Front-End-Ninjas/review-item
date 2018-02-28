756a07b43b836bd8838de984ac6b457e92c26f68
Dakaney <wayman_ng@yahoo.com>
2018-02-28 12:49:28 -0800

    test

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const Reviews = require('../server/schema.js');
-const seedDb = require('../seedData.js');
 const data = require('../dummyData.js');
-
-mongoose.connect('mongodb://localhost/reviews');
 
 const reviewSchema = mongoose.Schema({
   itemId: Number,
@@ -20,7 +17,23 @@ const reviewSchema = mongoose.Schema({
 
 describe('Mongo Database', () => {
   beforeAll(() => {
-    seedDb(data);
+    mongoose.connect('mongodb://localhost/reviews');
+    data.forEach((item) => {
+      for (let i = 0; i < item.reviews.length; i += 1) {
+        const insertReview = {
+          itemId: item.id,
+          username: item.reviews[i].username,
+          rating: item.reviews[i].rating,
+          comment: item.reviews[i].comment,
+          reviewTitle: item.reviews[i].reviewTitle,
+          reviewDate: item.reviews[i].date,
+          purchased: item.reviews[i].purchased,
+        };
+        Reviews.insertOne(insertReview).catch((err) => {
+          console.error(err);
+        });
+      }
+    });
   });
   afterAll(() => {
     mongoose.disconnect();
